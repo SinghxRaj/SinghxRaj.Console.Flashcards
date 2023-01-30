d48b5868ec8dd7afda2f067fbeb2bf917c6ab64e
RickySin11 <69612398+SinghxRaj@users.noreply.github.com>
2023-01-30 00:38:05 -0800

    Add pseudocode for Menu Controller Run method

--- SinghxRaj.Flashcards/SinghxRaj.Flashcards/Controller/MenuController.cs
+++ SinghxRaj.Flashcards/SinghxRaj.Flashcards/Controller/MenuController.cs
@@ -4,9 +4,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SinghxRaj.Flashcards.Controller
+namespace SinghxRaj.Flashcards.Controller;
+
+internal class MenuController
 {
-    internal class MenuController
+    public static void Run() 
     {
+        // Display introduction of program
+        // Display the options to the user
+        // Get option (validate it)
+        // Execute the option
     }
 }
